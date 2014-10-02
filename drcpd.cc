93625eb3f12aa3564386209cd12fe1b7c4ef59ae
Robert Tiemann <r.tiemann@ta-hifi.com>
2014-10-02 12:11:54 +0200

    Fixed "make dist", failed for empty translations.

--- drcpd.cc
+++ drcpd.cc
@@ -2,7 +2,16 @@
 #include <config.h>
 #endif /* HAVE_CONFIG_H */
 
+#include <stdio.h>
+
+#include "i18n.h"
+
 int main(int argc, char *argv[])
 {
+    i18n_init();
+
+    const char *dummy = _("dummy");
+    printf("%s\n", dummy);
+
     return 0;
 }
