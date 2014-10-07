TplusA/DRCPD

commit 2f7d79152f080519a635876d466158e055ec69be

Made List::Item an interface, TextItem specializes.
Now a more dynamic memory allocation is necessary, but at least not
all list items need a std::string member now (whose use may also
trigger some memory allocation).

ramlist.cc
@@ -21,7 +21,7 @@ List::Item *List::RamList::get_nonconst_item(unsigned int line)
     if(line >= get_number_of_items())
         return nullptr;
     else
-        return &items_[line];
+        return items_[line];
 }
 
 void List::RamList::set_parent_list(const List::ListIface *parent)
@@ -61,8 +61,8 @@ const List::ListIface *List::RamList::down(unsigned int line) const
     return item->down();
 }
 
-unsigned int List::RamList::append(Item &&item)
+unsigned int List::RamList::append(Item *item)
 {
-    items_.push_back(std::move(item));
+    items_.push_back(item);
     return items_.size() - 1;
 }

ramlist.hh
@@ -22,7 +22,7 @@ class RamList: public ListIface
     RamList &operator=(const RamList &);
 
     const ListIface *parent_list_;
-    std::vector<Item> items_;
+    std::vector<Item *> items_;
 
     Item *get_nonconst_item(unsigned int line);
 
@@ -39,8 +39,14 @@ class RamList: public ListIface
     const ListIface &up() const override;
     const ListIface *down(unsigned int line) const override;
 
-    unsigned int append(Item &&item);
+    unsigned int append(Item *item);
 };
+
+template <typename T>
+static unsigned int append(RamList *list, T &&item)
+{
+    return list->append(new T(std::move(item)));
+}
 
 };
 

list.cc
@@ -5,7 +5,7 @@
 #include "list.hh"
 #include "i18n.h"
 
-const char *List::Item::get_text() const
+const char *List::TextItem::get_text() const
 {
     return (!text_is_translatable_) ? text_.c_str() : _(text_.c_str());
 }

list.hh
@@ -21,28 +21,19 @@ class Item
     Item(const Item &);
     Item &operator=(const Item &);
 
-    std::string text_;
-    bool text_is_translatable_;
-
+  protected:
     unsigned int flags_;
     std::shared_ptr<ListIface> child_list_;
+
+    explicit Item(unsigned int flags):
+        flags_(flags),
+        child_list_(nullptr)
+    {}
 
   public:
     explicit Item(Item &&) = default;
 
-    explicit Item(unsigned int flags):
-        text_is_translatable_(false),
-        flags_(flags),
-        child_list_(nullptr)
-    {}
-
-    explicit Item(const char *text, bool text_is_translatable,
-                  unsigned int flags):
-        text_(text),
-        text_is_translatable_(text_is_translatable),
-        flags_(flags),
-        child_list_(nullptr)
-    {}
+    virtual ~Item() {}
 
     void set_child_list(const std::shared_ptr<ListIface> &list)
     {
@@ -53,6 +44,31 @@ class Item
     {
         return child_list_.get();
     }
+};
+
+class TextItem: public Item
+{
+  private:
+    TextItem(const TextItem &);
+    TextItem &operator=(const TextItem &);
+
+    std::string text_;
+    bool text_is_translatable_;
+
+  public:
+    explicit TextItem(TextItem &&) = default;
+
+    explicit TextItem(unsigned int flags):
+        Item(flags),
+        text_is_translatable_(false)
+    {}
+
+    explicit TextItem(const char *text, bool text_is_translatable,
+                      unsigned int flags):
+        Item(flags),
+        text_(text),
+        text_is_translatable_(text_is_translatable)
+    {}
 
     const char *get_text() const;
 };

tests/test_list.cc
@@ -79,12 +79,12 @@ void test_list_is_empty_on_startup(void)
 void test_add_single_list_item(void)
 {
     unsigned int line =
-        list->append(List::Item("Test entry", false, 0));
+        List::append(list.get(), List::TextItem("Test entry", false, 0));
 
     cut_assert_equal_uint(0, line);
     cut_assert_equal_uint(1, list->get_number_of_items());
 
-    const List::Item *item = list->get_item(line);
+    auto item = dynamic_cast<const List::TextItem *>(list->get_item(line));
 
     cut_assert_not_null(item);
     cut_assert_equal_string("Test entry", item->get_text());
@@ -98,7 +98,7 @@ static void append_items_to_list(const std::shared_ptr<List::RamList> &l,
 
     for(const char **s = strings; *s != nullptr; ++s)
     {
-        (void)l->append(List::Item(*s, false, 0));
+        (void)List::append(l.get(), List::TextItem(*s, false, 0));
         ++expected_size;
     }
 
@@ -113,7 +113,7 @@ void test_add_multiple_list_item(void)
 
     for(unsigned int i = 0; i < sizeof(strings) / sizeof(strings[0]) - 1; ++i)
     {
-        const List::Item *item = list->get_item(i);
+        auto item = dynamic_cast<const List::TextItem *>(list->get_item(i));
 
         cut_assert_not_null(item);
         cut_assert_equal_string(strings[i], item->get_text());
@@ -122,7 +122,7 @@ void test_add_multiple_list_item(void)
 
 void test_move_down_hierarchy_without_child_list_returns_null_list(void)
 {
-    (void)list->append(List::Item("Foo", false, 0));
+    (void)List::append(list.get(), List::TextItem("Foo", false, 0));
     cut_assert_null(list->down(0));
 }
 
@@ -151,19 +151,19 @@ void test_up_and_down_one_level_of_hierarchy(void)
     cut_assert_equal_pointer(list.get(), &second_list->up());
     cut_assert_equal_pointer(second_list.get(), list->down(0));
     cut_assert_equal_pointer(list.get(), &list->down(0)->up());
-    cut_assert_equal_string("second", list->down(0)->get_item(1)->get_text());
+    cut_assert_equal_string("second", dynamic_cast<const List::TextItem *>(list->down(0)->get_item(1))->get_text());
 
     cut_assert_equal_pointer(list.get(), &third_list->up());
     cut_assert_equal_pointer(third_list.get(), list->down(1));
     cut_assert_equal_pointer(list.get(), &list->down(1)->up());
-    cut_assert_equal_string("sixth", list->down(1)->get_item(2)->get_text());
+    cut_assert_equal_string("sixth", dynamic_cast<const List::TextItem *>(list->down(1)->get_item(2))->get_text());
 }
 
 void test_up_and_down_multiple_levels_of_hierarchy(void)
 {
     static const char *levels[] = { "Level 0", "Level 1", "Level 2", "Level 3", nullptr };
 
-    cut_assert_equal_uint(0, list->append(List::Item(levels[0], false, 0)));
+    cut_assert_equal_uint(0, List::append(list.get(), List::TextItem(levels[0], false, 0)));
 
     size_t idx = 1;
 
@@ -172,7 +172,8 @@ void test_up_and_down_multiple_levels_of_hierarchy(void)
         std::shared_ptr<List::ListIface> l(new List::RamList());
         List::RamList *ram_list = static_cast<List::RamList *>(l.get());
 
-        cut_assert_equal_uint(0, ram_list->append(List::Item(levels[idx],
+        cut_assert_equal_uint(0, List::append(ram_list,
+                                              List::TextItem(levels[idx],
                                                              false, 0)));
         cut_assert_true(prev->set_child_list(0, l));
 
@@ -188,7 +189,7 @@ void test_up_and_down_multiple_levels_of_hierarchy(void)
     {
         cut_assert_equal_pointer(const_prev, &l->up());
 
-        const List::Item *item = l->get_item(0);
+        auto item = dynamic_cast<const List::TextItem *>(l->get_item(0));
 
         cut_assert_not_null(item);
         cut_assert_equal_string(levels[idx], item->get_text());

commit b401ee59bbf6551a948d307402f1fbcf5c321108

RamList: Fixed memory leak introduced in 2f7d791.

ramlist.cc
@@ -6,6 +6,12 @@
 
 #include "ramlist.hh"
 
+List::RamList::~RamList()
+{
+    for(auto i : items_)
+        delete i;
+}
+
 unsigned int List::RamList::get_number_of_items() const
 {
     return items_.size();

ramlist.hh
@@ -28,6 +28,7 @@ class RamList: public ListIface
 
   public:
     explicit RamList(): parent_list_(this) {}
+    ~RamList();
 
     unsigned int get_number_of_items() const override;
 