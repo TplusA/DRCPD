6810f2e432d023b6665b08635bd8b648506d4129
Robert Tiemann <r.tiemann@ta-hifi.com>
2015-02-03 17:19:40 +0100

    Apply GPLv3 to source code.
    Change-Id: I4203181dd69e12cab30e653921ee3509eae360d5

--- drcpd.cc
+++ drcpd.cc
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2015  T+A elektroakustik GmbH & Co. KG
+ *
+ * This file is part of DRCPD.
+ *
+ * DRCPD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 3 as
+ * published by the Free Software Foundation.
+ *
+ * DRCPD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DRCPD.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #if HAVE_CONFIG_H
 #include <config.h>
 #endif /* HAVE_CONFIG_H */
--- tests/test_list.cc
+++ tests/test_list.cc
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2015  T+A elektroakustik GmbH & Co. KG
+ *
+ * This file is part of DRCPD.
+ *
+ * DRCPD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 3 as
+ * published by the Free Software Foundation.
+ *
+ * DRCPD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DRCPD.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <cppcutter.h>
 
 #include "ramlist.hh"
