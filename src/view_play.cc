TplusA/DRCPD

commit ac2f2f508e847b390f74d6b3a3e3df81b5f0e5bc

Remove unneeded characters from generated XML (#158).
For saving SPI bandwidth and to avoid transaction overhead (surely
there are situations with big data in which fewer transactions are
required now).

Change-Id: Icd2d32d3f04a24abc1c21734a4e44abb6a793a4b

src/view_play.cc
@@ -170,36 +170,36 @@ bool ViewPlay::View::write_xml(std::ostream &os, bool is_full_view)
 
     if((update_flags_ & update_flags_meta_data) != 0)
     {
-        os << "    <text id=\"artist\">"
+        os << "<text id=\"artist\">"
            << XmlEscape(md.values_[PlayInfo::MetaData::ARTIST])
-           << "</text>\n";
-        os << "    <text id=\"track\">"
+           << "</text>";
+        os << "<text id=\"track\">"
            << XmlEscape(md.values_[PlayInfo::MetaData::TITLE])
-           << "</text>\n";
-        os << "   <text id=\"alttrack\">"
+           << "</text>";
+        os << "text id=\"alttrack\">"
            << XmlEscape(mk_alt_track_name(md, 20))
-           << "</text>\n";
-        os << "    <text id=\"album\">"
+           << "</text>";
+        os << "<text id=\"album\">"
            << XmlEscape(md.values_[PlayInfo::MetaData::ALBUM])
-           << "</text>\n";
-        os << "    <text id=\"bitrate\">"
+           << "</text>";
+        os << "<text id=\"bitrate\">"
            << md.values_[PlayInfo::MetaData::BITRATE_NOM]
-           << "</text>\n";
+           << "</text>";
     }
 
     if((update_flags_ & update_flags_stream_position) != 0)
     {
         auto times = player_.get_times();
 
-        os << "    <value id=\"timet\">";
+        os << "<value id=\"timet\">";
         if(times.second >= std::chrono::milliseconds(0))
             os << std::chrono::duration_cast<std::chrono::seconds>(times.second).count();
-        os << "</value>\n";
+        os << "</value>";
 
         if(times.first >= std::chrono::milliseconds(0))
-            os << "    <value id=\"timep\">"
+            os << "<value id=\"timep\">"
                 << std::chrono::duration_cast<std::chrono::seconds>(times.first).count()
-                << "</value>\n";
+                << "</value>";
     }
 
     if((update_flags_ & update_flags_playback_state) != 0)
@@ -214,9 +214,9 @@ bool ViewPlay::View::write_xml(std::ostream &os, bool is_full_view)
 
         static_assert(sizeof(play_icon) / sizeof(play_icon[0]) == PlayInfo::Data::STREAM_STATE_LAST + 1, "Array has wrong size");
 
-        os << "    <icon id=\"play\">"
+        os << "<icon id=\"play\">"
            << play_icon[player_.get_assumed_stream_state()]
-           << "</icon>\n";
+           << "</icon>";
     }
 
     return true;

src/view.hh
@@ -253,12 +253,12 @@ class ViewIface
     virtual bool write_xml_begin(std::ostream &os, bool is_full_view)
     {
         os << "<" << (is_full_view ? "view" : "update") << " id=\""
-           << drcp_view_id_ << "\">\n";
+           << drcp_view_id_ << "\">";
 
         if(is_full_view)
         {
-            os << "    <text id=\"title\">" << XmlEscape(_(on_screen_name_)) << "</text>\n";
-            os << "    <text id=\"scrid\">" << int(drcp_screen_id_) << "</text>\n";
+            os << "<text id=\"title\">" << XmlEscape(_(on_screen_name_)) << "</text>";
+            os << "<text id=\"scrid\">" << int(drcp_screen_id_) << "</text>";
         }
 
         return true;

src/view_filebrowser.cc
@@ -138,7 +138,7 @@ ViewIface::InputResult ViewFileBrowser::View::input(DrcpCommand command)
 
 bool ViewFileBrowser::View::write_xml(std::ostream &os, bool is_full_view)
 {
-    os << "    <text id=\"cbid\">" << int(drcp_browse_id_) << "</text>\n";
+    os << "<text id=\"cbid\">" << int(drcp_browse_id_) << "</text>";
 
     size_t displayed_line = 0;
 
@@ -168,16 +168,16 @@ bool ViewFileBrowser::View::write_xml(std::ostream &os, bool is_full_view)
         if(it == navigation_.get_cursor())
             flags.push_back('s');
 
-        os << "    <text id=\"line" << displayed_line << "\" flag=\"" << flags << "\">"
-           << XmlEscape(item != nullptr ? item->get_text() : "-----") << "</text>\n";
+        os << "<text id=\"line" << displayed_line << "\" flag=\"" << flags << "\">"
+           << XmlEscape(item != nullptr ? item->get_text() : "-----") << "</text>";
 
         ++displayed_line;
     }
 
-    os << "    <value id=\"listpos\" min=\"1\" max=\""
+    os << "<value id=\"listpos\" min=\"1\" max=\""
        << navigation_.get_total_number_of_visible_items() << "\">"
        << navigation_.get_line_number_by_cursor() + 1
-       << "</value>\n";
+       << "</value>";
 
     return true;
 }

commit 5be4c36b62ef2628076c042d2a84bb5847d74860

Fix XML output, missing '<' for alttrack tag (#173).
Bug was introduced in ac2f2f508e847b390f74d6b3a3e3df81b5f0e5bc.

Change-Id: I3a6ef7aa8f406e26b3a3d2c3c5b85749f40b62e7

src/view_play.cc
@@ -176,7 +176,7 @@ bool ViewPlay::View::write_xml(std::ostream &os, bool is_full_view)
         os << "<text id=\"track\">"
            << XmlEscape(md.values_[PlayInfo::MetaData::TITLE])
            << "</text>";
-        os << "text id=\"alttrack\">"
+        os << "<text id=\"alttrack\">"
            << XmlEscape(mk_alt_track_name(md, 20))
            << "</text>";
         os << "<text id=\"album\">"