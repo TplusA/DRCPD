TplusA/DRCPD

commit 48928c36dc14d058274d0c5be5d8fad22395fa68

Replace state flags by state enumeration.
Change-Id: I36e981abc7bb961860fc9d6bf7bec9ac6382c7c9

src/dbus_async.hh
@@ -25,19 +25,26 @@
 namespace DBus
 {
 
+enum class AsyncResult
+{
+    INITIALIZED,
+    IN_PROGRESS,
+    DONE,
+    CANCELED,
+    FAILED,
+};
+
 class AsyncCall_
 {
   private:
     bool is_zombie_;
 
   protected:
-    bool has_completed_;
-    bool was_successful_;
+    AsyncResult call_state_;
 
     explicit AsyncCall_():
         is_zombie_(false),
-        has_completed_(false),
-        was_successful_(false)
+        call_state_(AsyncResult::INITIALIZED)
     {}
 
   public:
@@ -46,8 +53,56 @@ class AsyncCall_
 
     virtual ~AsyncCall_() {}
 
-    bool is_complete() const { return has_completed_; }
-    bool success() const { return was_successful_; }
+    bool is_running() const
+    {
+        switch(call_state_)
+        {
+          case AsyncResult::IN_PROGRESS:
+            return true;
+
+          case AsyncResult::INITIALIZED:
+          case AsyncResult::DONE:
+          case AsyncResult::CANCELED:
+          case AsyncResult::FAILED:
+            break;
+        }
+
+        return false;
+    }
+
+    bool is_complete() const
+    {
+        switch(call_state_)
+        {
+          case AsyncResult::DONE:
+          case AsyncResult::CANCELED:
+          case AsyncResult::FAILED:
+            return true;
+
+          case AsyncResult::INITIALIZED:
+          case AsyncResult::IN_PROGRESS:
+            break;
+        }
+
+        return false;
+    }
+
+    bool success() const
+    {
+        switch(call_state_)
+        {
+          case AsyncResult::DONE:
+            return true;
+
+          case AsyncResult::INITIALIZED:
+          case AsyncResult::IN_PROGRESS:
+          case AsyncResult::CANCELED:
+          case AsyncResult::FAILED:
+            break;
+        }
+
+        return false;
+    }
 
     /*!
      * If the asynchronous call failed, clean up already.
@@ -185,13 +240,16 @@ class AsyncCall: public DBus::AsyncCall_
 
         g_object_unref(G_OBJECT(cancellable_));
 
-        if(was_successful_)
+        if(success())
             destroy_result_fn_(return_value_);
     }
 
     template <typename DBusMethodType, typename... Args>
     void invoke(DBusMethodType dbus_method, Args&&... args)
     {
+        log_assert(!is_running());
+
+        call_state_ = AsyncResult::IN_PROGRESS;
         dbus_method(proxy_, args..., cancellable_, async_ready_trampoline, this);
     }
 
@@ -253,8 +311,10 @@ class AsyncCall: public DBus::AsyncCall_
 
     void ready(ProxyType *proxy, GAsyncResult *res)
     {
-        has_completed_ = true;
-        put_result_fn_(was_successful_, promise_, proxy_, res, error_);
+        bool was_successful = false;
+
+        put_result_fn_(was_successful, promise_, proxy_, res, error_);
+        call_state_ = was_successful ? AsyncResult::DONE : AsyncResult::FAILED;
     }
 };
 

commit 95730bfbd5d77fcfff6d7484162b34c39ffddcd8

Further refine access to DBus::AsyncCall.
Waiting for the result and getting the actual result are two separate
operations now. The latter operation can be called anytime and returns
the current status of the operation.

This fixes a synchronization issue introduced in 48928c3.

Change-Id: I8535c0f85ec9326624d138106060899e3dbc4885

src/dbus_async.hh
@@ -107,6 +107,12 @@ class AsyncCall_
     /*!
      * If the asynchronous call failed, clean up already.
      *
+     * \param call
+     *     An asynchronous call context that shall be freed. Do \e not delete
+     *     or use the passed object if this function returns \c true. Delete
+     *     the passed \p call after use if and only if this function returns
+     *     \c false.
+     *
      * \retval True
      *     The asynchronous D-Bus call has failed and the passed object has
      *     either been deleted, or turned into a zombie and will be deleted at
@@ -115,7 +121,7 @@ class AsyncCall_
      * \retval False
      *     The asynchronous D-Bus call was successful and the result is usable.
      *     The caller must delete the object when it is done with it. Note that
-     *     the result returned by #DBus::AsyncCall::wait_for_result() returns a
+     *     the result returned by #DBus::AsyncCall::get_result() returns a
      *     \e reference to an object owned by the #DBus::AsyncCall object.
      *     Deleting the #DBus::AsyncCall object also destroys the result.
      */
@@ -124,7 +130,7 @@ class AsyncCall_
         if(call->success())
             return false;
 
-        if(call->is_complete())
+        if(!call->is_running() || call->is_complete())
             delete call;
         else
         {
@@ -207,9 +213,11 @@ class AsyncCall: public DBus::AsyncCall_
      *     obtain the results, using the passed \c GAsyncResult and \c GError
      *     pointers as parameters; (2) assign the return value of \c _finish()
      *     to the passed \c bool reference; (3) pack the results returned by
-     *     the \c _finish() function into the passed \c std::promise. The final
-     *     step, calling \c set_value() for the \c std::promise, \e must be the
-     *     last statement in the function to ensure correct synchronization.
+     *     the \c _finish() function into the passed \c std::promise, or, in
+     *     case of failure, pack fallback values into the \c std::promise. The
+     *     final step, calling \c set_value() for the \c std::promise, \e must
+     *     be the last statement in the function to ensure correct
+     *     synchronization.
      *
      * \param destroy_result
      *     Called from the #AsyncCall destructor to free the result placed into
@@ -259,11 +267,12 @@ class AsyncCall: public DBus::AsyncCall_
      * Call #DBus::AsyncCall::cleanup_if_failed() after this function has
      * returned. Do not use the result before doing this.
      */
-    const PromiseReturnType &wait_for_result()
+    AsyncResult wait_for_result()
     {
+        log_assert(is_running());
+
         auto future(promise_.get_future());
-        if(!future.valid())
-            return return_value_;
+        log_assert(future.valid());
 
         if(!g_cancellable_is_cancelled(cancellable_))
         {
@@ -280,6 +289,12 @@ class AsyncCall: public DBus::AsyncCall_
         if(!g_cancellable_is_cancelled(cancellable_))
             return_value_ = future.get();
 
+        return call_state_;
+    }
+
+    const PromiseReturnType &get_result(AsyncResult &async_result) const
+    {
+        log_assert(success());
         return return_value_;
     }
 

src/playbackmode_state.cc
@@ -170,9 +170,15 @@ static std::string get_selected_uri(ID::List list_id, unsigned int item_id,
         },
         [&abort_enqueue] () { return abort_enqueue.may_continue(); });
 
+    if(async_call == nullptr)
+    {
+        msg_out_of_memory("asynchronous D-Bus call");
+        return empty_string;
+    }
+
     async_call->invoke(tdbus_lists_navigation_call_get_uris,
                        list_id.get_raw_id(), item_id);
-    const auto &result(async_call->wait_for_result());
+    async_call->wait_for_result();
 
     if(AsyncCallType::cleanup_if_failed(async_call))
     {
@@ -181,6 +187,9 @@ static std::string get_selected_uri(ID::List list_id, unsigned int item_id,
 
         return empty_string;
     }
+
+    DBus::AsyncResult async_result;
+    const auto &result(async_call->get_result(async_result));
 
     const ListError error(std::get<0>(result));
     gchar **const uri_list(std::get<1>(result));