TplusA/DRCPD

commit eed35e226aea94dfcfc9ba9266facf1064869531

Adapt to changed de.tahifi.Lists.Navigation.* interfaces.
The software compiles, but it doesn't make use of search forms yet.

Change-Id: I6f21181c664e86e39d3e7fc5ad0f564b695011b3

src/view_filebrowser.cc
@@ -29,9 +29,9 @@
 #include "messages.h"
 
 List::Item *ViewFileBrowser::construct_file_item(const char *name,
-                                                 bool is_directory)
+                                                 ListItemKind kind)
 {
-    return new FileItem(name, 0, is_directory);
+    return new FileItem(name, 0, kind);
 }
 
 bool ViewFileBrowser::View::init()
@@ -72,7 +72,7 @@ ViewIface::InputResult ViewFileBrowser::View::input(DrcpCommand command)
 
         if(item)
         {
-            if(item->is_directory())
+            if(item->get_kind().is_directory())
             {
                 if(point_to_child_directory())
                     return InputResult::UPDATE_NEEDED;
@@ -172,10 +172,29 @@ bool ViewFileBrowser::View::write_xml(std::ostream &os, bool is_full_view)
 
         std::string flags;
 
-        if(item->is_directory())
+        switch(item->get_kind().get())
+        {
+          case ListItemKind::DIRECTORY:
             flags.push_back('d');
-        else
+            break;
+
+          case ListItemKind::SERVER:
+          case ListItemKind::STORAGE_DEVICE:
+            flags.push_back('S');
+            break;
+
+          case ListItemKind::REGULAR_FILE:
             flags.push_back('p');
+            break;
+
+          case ListItemKind::LOCKED:
+            flags.push_back('L');
+            break;
+
+          case ListItemKind::OPAQUE:
+          case ListItemKind::SEARCH_FORM:
+            break;
+        }
 
         if(it == navigation_.get_cursor())
             flags.push_back('s');
@@ -220,7 +239,7 @@ bool ViewFileBrowser::View::serialize(DcpTransaction &dcpd, std::ostream *debug_
             *debug_os << "    ";
 
         if(item != nullptr)
-            *debug_os << (item->is_directory() ? "Dir " : "File") << " " << it << ": "
+            *debug_os << "Type " << item->get_kind().get_raw_code() << " " << it << ": "
                       << item->get_text() << std::endl;
         else
             *debug_os << "*NULL ENTRY* " << it << std::endl;
@@ -360,6 +379,8 @@ bool ViewFileBrowser::View::point_to_child_directory()
           case ListError::Code::PROTOCOL:
           case ListError::Code::AUTHENTICATION:
           case ListError::Code::INCONSISTENT:
+          case ListError::Code::PERMISSION_DENIED:
+          case ListError::Code::NOT_SUPPORTED:
             /* problem: stay right there where you are */
             msg_info("Go to child list error, stay here: %s", e.what());
             return false;
@@ -414,6 +435,8 @@ bool ViewFileBrowser::View::point_to_parent_link()
           case ListError::Code::INVALID_ID:
           case ListError::Code::AUTHENTICATION:
           case ListError::Code::INCONSISTENT:
+          case ListError::Code::PERMISSION_DENIED:
+          case ListError::Code::NOT_SUPPORTED:
             /* funny problem: better return to root directory */
             break;
         }

src/Makefile.am
@@ -31,7 +31,9 @@ drcpd_SOURCES = \
     playinfo.hh idtypes.hh stream_id.h stream_id.hh \
     player.hh playbackmode.hh playbackmode_state.hh \
     $(DBUS_IFACES)/de_tahifi_lists_errors.hh \
-    $(DBUS_IFACES)/de_tahifi_lists_errors.h
+    $(DBUS_IFACES)/de_tahifi_lists_errors.h \
+    $(DBUS_IFACES)/de_tahifi_lists_item_kinds.hh \
+    $(DBUS_IFACES)/de_tahifi_lists_item_kinds.h
 
 DBUS_IFACES = $(top_srcdir)/dbus_interfaces
 

src/dbuslist.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015  T+A elektroakustik GmbH & Co. KG
+ * Copyright (C) 2015, 2016  T+A elektroakustik GmbH & Co. KG
  *
  * This file is part of DRCPD.
  *
@@ -85,6 +85,8 @@ static unsigned int query_list_size(tdbuslistsNavigation *proxy,
       case ListError::Code::PROTOCOL:
       case ListError::Code::AUTHENTICATION:
       case ListError::Code::INCONSISTENT:
+      case ListError::Code::PERMISSION_DENIED:
+      case ListError::Code::NOT_SUPPORTED:
         msg_error(0, LOG_NOTICE,
                   "Error while obtaining size of list ID %u: %s",
                   list_id.get_raw_id(), error.to_string());
@@ -167,15 +169,15 @@ static void fill_cache_list(List::RamList &items,
         return;
 
     gchar *name;
-    gboolean is_directory;
+    uint8_t item_kind;
 
-    while(g_variant_iter_next(&iter, "(sb)", &name, &is_directory))
+    while(g_variant_iter_next(&iter, "(sy)", &name, &item_kind))
     {
         if(replace_mode)
             items.replace(cache_list_index++,
-                          new_item_fn(name, !!is_directory));
+                          new_item_fn(name, ListItemKind(item_kind)));
         else
-            items.append(new_item_fn(name, !!is_directory));
+            items.append(new_item_fn(name, ListItemKind(item_kind)));
 
         g_free(name);
     }

src/dbuslist.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015  T+A elektroakustik GmbH & Co. KG
+ * Copyright (C) 2015, 2016  T+A elektroakustik GmbH & Co. KG
  *
  * This file is part of DRCPD.
  *
@@ -24,6 +24,7 @@
 #include "ramlist.hh"
 #include "messages.h"
 #include "dbuslist_exception.hh"
+#include "de_tahifi_lists_item_kinds.hh"
 
 /*!
  * \addtogroup dbus_list Lists with contents filled directly from D-Bus
@@ -40,7 +41,7 @@ namespace List
 class DBusList: public ListIface
 {
   public:
-    typedef List::Item *(*const NewItemFn)(const char *name, bool is_directory);
+    typedef List::Item *(*const NewItemFn)(const char *name, ListItemKind kind);
 
   private:
     tdbuslistsNavigation *const dbus_proxy_;

src/playbackmode_state.cc
@@ -367,7 +367,7 @@ bool Playback::State::try_start()
     if(item == nullptr)
         return false;
 
-    if(!item->is_directory())
+    if(!item->get_kind().is_directory())
         return true;
 
     if(mode_.get() != Playback::Mode::LINEAR)
@@ -418,6 +418,28 @@ bool Playback::State::start(const List::DBusList &user_list,
     }
 
     return false;
+}
+
+static inline bool can_queue_item(const ViewFileBrowser::FileItem &item)
+{
+    switch(item.get_kind().get())
+    {
+      case ListItemKind::OPAQUE:
+      case ListItemKind::REGULAR_FILE:
+        return true;
+
+      case ListItemKind::LOCKED:
+      case ListItemKind::DIRECTORY:
+      case ListItemKind::SERVER:
+      case ListItemKind::STORAGE_DEVICE:
+      case ListItemKind::SEARCH_FORM:
+        return false;
+    }
+
+    BUG("Unhandled item kind %u, still pushing it to streamplayer",
+        item.get_kind().get_raw_code());
+
+    return true;
 }
 
 bool Playback::State::enqueue_next(StreamInfo &sinfo, bool skip_to_next,
@@ -477,7 +499,7 @@ bool Playback::State::enqueue_next(StreamInfo &sinfo, bool skip_to_next,
             return queued_for_immediate_playback;
         }
 
-        if(!item->is_directory())
+        if(can_queue_item(*item))
         {
             const unsigned int current_line = navigation_.get_cursor();
             const ID::OurStream fallback_title_id =
@@ -598,7 +620,7 @@ bool Playback::State::find_next(const List::TextItem *directory)
 
     auto item = dynamic_cast<const ViewFileBrowser::FileItem *>(directory);
 
-    if(item != nullptr && item->is_directory() && try_descend())
+    if(item != nullptr && item->get_kind().is_directory() && try_descend())
         return true;
 
     while(true)

src/view_filebrowser.hh
@@ -27,6 +27,7 @@
 #include "dbus_iface.h"
 #include "dbus_iface_deep.h"
 #include "idtypes.hh"
+#include "de_tahifi_lists_item_kinds.hh"
 
 namespace Playback { class Player; }
 
@@ -43,7 +44,7 @@ namespace Playback { class Player; }
 namespace ViewFileBrowser
 {
 
-List::Item *construct_file_item(const char *name, bool is_directory);
+List::Item *construct_file_item(const char *name, ListItemKind kind);
 
 class View: public ViewIface
 {
@@ -149,7 +150,7 @@ class View: public ViewIface
 class FileItem: public List::TextItem
 {
   private:
-    bool is_directory_;
+    ListItemKind kind_;
 
   public:
     FileItem(const FileItem &) = delete;
@@ -157,16 +158,13 @@ class FileItem: public List::TextItem
     explicit FileItem(FileItem &&) = default;
 
     explicit FileItem(const char *text, unsigned int flags,
-                      bool item_is_directory):
+                      ListItemKind item_kind):
         List::Item(flags),
         List::TextItem(text, true, flags),
-        is_directory_(item_is_directory)
+        kind_(item_kind)
     {}
 
-    bool is_directory() const
-    {
-        return is_directory_;
-    }
+    ListItemKind get_kind() const { return kind_; }
 };
 
 };

commit d631a469fe271d299a16133aa89be098876c9ecc

Fix debug output broken in eed35e2.
Change-Id: I57f0d3a93a9772671948e299e60fadacd3e8adaf

src/view_filebrowser.cc
@@ -909,7 +909,8 @@ void ViewFileBrowser::View::serialize(DCP::Queue &queue, DCP::Queue::Mode mode,
             *debug_os << "    ";
 
         if(item != nullptr)
-            *debug_os << "Type " << item->get_kind().get_raw_code() << " " << it << ": "
+            *debug_os << "Type " << (unsigned int)item->get_kind().get_raw_code()
+                      << " " << it << ": "
                       << item->get_text() << std::endl;
         else
             *debug_os << "*NULL ENTRY* " << it << std::endl;