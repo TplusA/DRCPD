TplusA/DRCPD

commit 083d85609f5f5acff6f217861df87c3826c138c5

Send partial view update on NowPlaying for same stream (#554).
Before, we've sent full view updates for NowPlaying (emitted by stream
player for seeks and speed changes). This may have confused the SPI
slave (though it shouldn't as full updates may occur anytime during
view switches), so it has been changed.

Change-Id: I42cadd7cb82a0ca9c6bf159d4ff924a95fb035bd

src/view_play.cc
@@ -266,9 +266,14 @@ ViewPlay::View::process_event(UI::ViewEventID event_id,
             player_data_.set_stream_state(stream_id, Player::StreamState::PLAYING);
             player_control_.play_notification(stream_id, switched_stream);
 
-            msg_info("Play view: stream started, %s",
+            msg_info("Play view: stream %s, %s",
+                     switched_stream ? "started" : "updated",
                      is_visible_ ? "send screen update" : "but view is invisible");
-            view_manager_->serialize_view_if_active(this, DCP::Queue::Mode::FORCE_ASYNC);
+
+            if(switched_stream)
+                view_manager_->serialize_view_if_active(this, DCP::Queue::Mode::FORCE_ASYNC);
+            else
+                view_manager_->update_view_if_active(this, DCP::Queue::Mode::FORCE_ASYNC);
 
             if(!queue_is_full)
                 player_control_.need_next_item_hint(false);

commit c9b30794017657e3a54c2b4347cb6402e89dd594

Fix play icon on resume from pause state (#559).
Bug was introduced in 083d85609f5f5acff6f217861df87c3826c138c5.

Change-Id: I790a2f9d0ab21fad24c0e4dcd30ad754a175d422

src/view_play.cc
@@ -273,7 +273,10 @@ ViewPlay::View::process_event(UI::ViewEventID event_id,
             if(switched_stream)
                 view_manager_->serialize_view_if_active(this, DCP::Queue::Mode::FORCE_ASYNC);
             else
+            {
+                add_update_flags(UPDATE_FLAGS_PLAYBACK_STATE);
                 view_manager_->update_view_if_active(this, DCP::Queue::Mode::FORCE_ASYNC);
+            }
 
             if(!queue_is_full)
                 player_control_.need_next_item_hint(false);