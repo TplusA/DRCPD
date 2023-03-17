TplusA/DRCPD

commit e3a25a12c3960977078d62f0474861a24ab3915d

Audio sources: Fix selection protocol implementation.
The D-Bus call completions for audio source selection and deselection
must be sent back to the audio source manager only after we are done
attaching to/detaching from the player (audio path manager waits for completion
and synchronizes its actions based on completion, so time of completion must
not be implemented in a sloppy way). We didn't do this, however, and notified
completion way too early. This caused us to send stop requests to the stream
player at times when we shouldn't because audio source selection already went
on and another process already took over the player.

This ultimately led to various situations where some other player
controller waited infinitely for a start notification. Audio source selection
was generally unreliable because of this bug.

Change-Id: I67fe13a0c110eb3f08f4a0e834946fa4cc0668f3

src/dbus_handlers.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015, 2016, 2017, 2018  T+A elektroakustik GmbH & Co. KG
+ * Copyright (C) 2015--2019  T+A elektroakustik GmbH & Co. KG
  *
  * This file is part of DRCPD.
  *
@@ -671,14 +671,18 @@ gboolean dbusmethod_audiopath_source_selected_on_hold(tdbusaupathSource *object,
 {
     enter_audiopath_source_handler(invocation);
 
-    auto *data = static_cast<DBus::SignalData *>(user_data);
+    Guard guard(
+        [object, invocation] ()
+        {
+            tdbus_aupath_source_complete_selected_on_hold(object, invocation);
+        });
+    auto params = UI::Events::mk_params<UI::EventID::AUDIO_SOURCE_SELECTED>(
+                        source_id, true, std::move(guard));
 
     /* views are switched by the player as seen necessary */
-    auto params = UI::Events::mk_params<UI::EventID::AUDIO_SOURCE_SELECTED>(source_id, true);
+    auto *data = static_cast<DBus::SignalData *>(user_data);
     data->event_sink_.store_event(UI::EventID::AUDIO_SOURCE_SELECTED,
                                   std::move(params));
-
-    tdbus_aupath_source_complete_selected_on_hold(object, invocation);
 
     return TRUE;
 }
@@ -691,14 +695,18 @@ gboolean dbusmethod_audiopath_source_selected(tdbusaupathSource *object,
 {
     enter_audiopath_source_handler(invocation);
 
-    auto *data = static_cast<DBus::SignalData *>(user_data);
+    Guard guard(
+        [object, invocation] ()
+        {
+            tdbus_aupath_source_complete_selected(object, invocation);
+        });
+    auto params = UI::Events::mk_params<UI::EventID::AUDIO_SOURCE_SELECTED>(
+                        source_id, true, std::move(guard));
 
     /* views are switched by the player as seen necessary */
-    auto params = UI::Events::mk_params<UI::EventID::AUDIO_SOURCE_SELECTED>(source_id, false);
+    auto *data = static_cast<DBus::SignalData *>(user_data);
     data->event_sink_.store_event(UI::EventID::AUDIO_SOURCE_SELECTED,
                                   std::move(params));
-
-    tdbus_aupath_source_complete_selected(object, invocation);
 
     return TRUE;
 }
@@ -711,14 +719,18 @@ gboolean dbusmethod_audiopath_source_deselected(tdbusaupathSource *object,
 {
     enter_audiopath_source_handler(invocation);
 
-    auto *data = static_cast<DBus::SignalData *>(user_data);
+    Guard guard(
+        [object, invocation] ()
+        {
+            tdbus_aupath_source_complete_deselected(object, invocation);
+        });
+    auto params = UI::Events::mk_params<UI::EventID::AUDIO_SOURCE_DESELECTED>(
+                        source_id, std::move(guard));
 
     /* views are switched by the player as seen necessary */
-    auto params = UI::Events::mk_params<UI::EventID::AUDIO_SOURCE_DESELECTED>(source_id);
+    auto *data = static_cast<DBus::SignalData *>(user_data);
     data->event_sink_.store_event(UI::EventID::AUDIO_SOURCE_DESELECTED,
                                   std::move(params));
-
-    tdbus_aupath_source_complete_deselected(object, invocation);
 
     return TRUE;
 }

src/Makefile.am
@@ -41,7 +41,7 @@ drcpd_SOURCES = \
     configuration.hh configuration_base.hh configuration_changed.hh \
     configuration_settings.hh inifile.h configuration_drcpd.hh \
     configuration_i18n.hh \
-    ui_parameters.hh ui_parameters_predefined.hh search_parameters.hh \
+    ui_parameters.hh ui_parameters_predefined.hh guard.hh search_parameters.hh \
     player_control.hh player_control.cc \
     player_data.hh player_data.cc \
     player_stopped_reason.hh playback_modes.hh \
@@ -118,7 +118,7 @@ libviews_la_SOURCES = \
     cacheenforcer.hh cacheenforcer.cc \
     gvariantwrapper.hh gvariantwrapper.cc \
     airable_links.hh airable_links.cc \
-    ui_parameters.hh ui_parameters_predefined.hh search_parameters.hh \
+    ui_parameters.hh ui_parameters_predefined.hh guard.hh search_parameters.hh \
     playback_modes.hh \
     search_algo.hh \
     actor_id.h \
@@ -134,7 +134,7 @@ libdcp_transaction_la_CFLAGS = $(AM_CFLAGS)
 libdcp_transaction_la_CXXFLAGS = $(AM_CXXFLAGS)
 
 libdbus_handlers_la_SOURCES = \
-    dbus_handlers.h dbus_handlers.hh dbus_handlers.cc \
+    dbus_handlers.h dbus_handlers.hh dbus_handlers.cc guard.hh \
     messages_dbus.h messages_dbus.c \
     actor_id.h
 libdbus_handlers_la_CFLAGS = $(AM_CFLAGS)

src/guard.hh
@@ -1,0 +1,76 @@
+/*
+ * Copyright (C) 2019  T+A elektroakustik GmbH & Co. KG
+ *
+ * This file is part of DRCPD.
+ *
+ * DRCPD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 3 as
+ * published by the Free Software Foundation.
+ *
+ * DRCPD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DRCPD.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef GUARD_HH
+#define GUARD_HH
+
+#include <functional>
+
+/*!
+ * Simple guard object for calling a function when the guard is destroyed.
+ *
+ * The wrapped function is guaranteed to be called when the guard object is
+ * destroyed. This is useful for cleanups or similar actions that must be
+ * executed after certain other actions have been executed. This may sound much
+ * like a custom deleter, but a #Guard is neither tied to \c unique_ptr nor to
+ * the life time of objects.
+ *
+ * Of course, the situations pictured above could always be implemented by
+ * calls of explicit callback functions. It is, however, easily possible to
+ * forget to add the corresponding calls to the code, especially if the code is
+ * convoluted or throws exceptions. Moving a #Guard object to the code in
+ * question is much safer because the wrapped function is going to be called
+ * for sure as soon as the object is destroyed when it goes out of scope.
+ *
+ * \note
+ *     The wrapped function should not throw exceptions. If it does, then the
+ *     exception will be caught and silently thrown away.
+ */
+class Guard
+{
+  private:
+    std::function<void()> fn_;
+
+  public:
+    Guard(const Guard &) = delete;
+    Guard &operator=(const Guard &) = delete;
+
+    Guard(Guard &&) = default;
+    Guard &operator=(Guard &&) = default;
+
+    explicit Guard(std::function<void()> &&fn):
+        fn_(std::move(fn))
+    {}
+
+    ~Guard()
+    {
+        if(fn_ != nullptr)
+        {
+            try
+            {
+                fn_();
+            }
+            catch(...)
+            {
+                /* catch 'em all, ignore 'em all */
+            }
+        }
+    }
+};
+
+#endif /* !GUARD_HH */

src/ui_parameters_predefined.hh
@@ -33,6 +33,7 @@
 #include "gvariantwrapper.hh"
 #include "i18nstring.hh"
 #include "de_tahifi_lists_errors.hh"
+#include "guard.hh"
 
 namespace UI
 {
@@ -51,13 +52,13 @@ struct ParamTraits<EventID::CONFIGURATION_UPDATED>
 template <>
 struct ParamTraits<EventID::AUDIO_SOURCE_SELECTED>
 {
-    using PType = SpecificParameters<std::tuple<const std::string, const bool>>;
+    using PType = SpecificParameters<std::tuple<const std::string, const bool, Guard>>;
 };
 
 template <>
 struct ParamTraits<EventID::AUDIO_SOURCE_DESELECTED>
 {
-    using PType = SpecificParameters<const std::string>;
+    using PType = SpecificParameters<std::tuple<const std::string, Guard>>;
 };
 
 template <>

src/view_play.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015, 2016, 2017, 2018  T+A elektroakustik GmbH & Co. KG
+ * Copyright (C) 2015--2019  T+A elektroakustik GmbH & Co. KG
  *
  * This file is part of DRCPD.
  *
@@ -619,7 +619,8 @@ ViewPlay::View::process_event(UI::ViewEventID event_id,
 
             Busy::clear(Busy::DirectSource::WAITING_FOR_APPLIANCE_AUDIO);
 
-            const std::string &ausrc_id(params->get_specific());
+            const auto &plist = params->get_specific();
+            const std::string &ausrc_id(std::get<0>(plist));
 
             if(!player_control_.source_deselected_notification(&ausrc_id))
             {

commit 37c77c404dc7d0fdc9cddb41d5389b23c9bab4d4

audio sources: Fix deferred audio source selection.
Bug was introduced in e3a25a12c3960977078d62f0474861a24ab3915d.

Change-Id: I7e233a3f91e8ed84ce1326ffa27da46a66d9e060

src/dbus_handlers.cc
@@ -888,7 +888,7 @@ gboolean dbusmethod_audiopath_source_selected(tdbusaupathSource *object,
             tdbus_aupath_source_complete_selected(object, invocation);
         });
     auto params = UI::Events::mk_params<UI::EventID::AUDIO_SOURCE_SELECTED>(
-                        source_id, true, std::move(guard));
+                        source_id, false, std::move(guard));
 
     /* views are switched by the player as seen necessary */
     auto *data = static_cast<DBus::SignalData *>(user_data);