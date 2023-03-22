TplusA/DRCPD

commit bd6d3fa66dad27aba97adfe6e009588fc6206d0d

views: Stable busy flag, and only sent to SPI slave if changed.
By "stable" we meant that we don't query the busy state when the view
is serialized, but we use the busy state from when the state change
was notified. This prevents race conditions and allows to omit sending
unchanged busy state in screen updates.

Change-Id: I3d6e64990607560431b3a37a91f76e4584aa1cd0

src/dcp_transaction_queue.cc
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2016, 2017, 2019, 2020, 2022  T+A elektroakustik GmbH & Co. KG
+ * Copyright (C) 2023  T+A elektroakustik GmbH & Co. KG
  *
  * This file is part of DRCPD.
  *
@@ -32,7 +33,8 @@ std::function<void(bool)> DCP::Queue::configure_timeout_callback;
 std::function<void()> DCP::Queue::schedule_async_processing_callback;
 
 void DCP::Queue::add(ViewSerializeBase *view,
-                     bool is_full_serialize, uint32_t view_update_flags)
+                     bool is_full_serialize, uint32_t view_update_flags,
+                     const Maybe<bool> &is_busy)
 {
     LOGGED_LOCK_CONTEXT_HINT;
     std::lock_guard<LoggedLock::Mutex> lock(q_.lock_);
@@ -44,7 +46,8 @@ void DCP::Queue::add(ViewSerializeBase *view,
                                 }));
 
     if(it == q_.data_.end())
-        q_.data_.emplace_back(new Data(view, is_full_serialize, view_update_flags));
+        q_.data_.emplace_back(std::make_unique<Data>(view, is_full_serialize,
+                                                     view_update_flags, is_busy));
     else
     {
         auto &d = *it;
@@ -53,6 +56,14 @@ void DCP::Queue::add(ViewSerializeBase *view,
             d->is_full_serialize_ = is_full_serialize;
 
         d->view_update_flags_ |= view_update_flags;
+
+        if(is_busy.is_known())
+        {
+            if(d->busy_flag_.is_known())
+                d->busy_flag_.set_unknown();
+            else
+                d->busy_flag_ = is_busy;
+        }
     }
 }
 

src/Makefile.am
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2015--2021  T+A elektroakustik GmbH & Co. KG
+# Copyright (C) 2015--2021, 2023  T+A elektroakustik GmbH & Co. KG
 #
 # This file is part of DRCPD.
 #
@@ -30,7 +30,7 @@ drcpd_SOURCES = \
     timeout.cc timeout.hh \
     os.c os.h named_pipe.c named_pipe.h fdstreambuf.hh \
     dbus_iface.cc dbus_iface.hh dbus_iface_proxies.hh dbus_handlers.hh \
-    dbus_async.hh \
+    dbus_async.hh maybe.hh \
     rnfcall.hh rnfcall_state.hh rnfcall_cookiecall.hh rnfcall_death_row.hh \
     rnfcall_get_list_id.hh rnfcall_get_location_trace.hh \
     rnfcall_get_range.hh rnfcall_get_ranked_stream_links.hh \
@@ -148,7 +148,8 @@ libviews_la_CXXFLAGS = $(AM_CXXFLAGS)
 
 libdcp_transaction_la_SOURCES = \
     dcp_transaction.cc dcp_transaction.hh \
-    dcp_transaction_queue.cc dcp_transaction_queue.hh
+    dcp_transaction_queue.cc dcp_transaction_queue.hh \
+    maybe.hh
 libdcp_transaction_la_CFLAGS = $(AM_CFLAGS)
 libdcp_transaction_la_CXXFLAGS = $(AM_CXXFLAGS)
 

src/dcp_transaction_queue.hh
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2016, 2017, 2019, 2020  T+A elektroakustik GmbH & Co. KG
+ * Copyright (C) 2023  T+A elektroakustik GmbH & Co. KG
  *
  * This file is part of DRCPD.
  *
@@ -23,6 +24,7 @@
 #define DCP_TRANSACTION_QUEUE_HH
 
 #include "dcp_transaction.hh"
+#include "maybe.hh"
 #include "messages.h"
 #include "logged_lock.hh"
 
@@ -74,15 +76,17 @@ class Queue: public QueueIntrospectionIface
       public:
         ViewSerializeBase *const view_;
         uint32_t view_update_flags_;
+        Maybe<bool> busy_flag_;
         bool is_full_serialize_;
 
         Data(const Data &) = delete;
         Data &operator=(const Data &) = delete;
 
         explicit Data(ViewSerializeBase *view, bool is_full_serialize,
-                      uint32_t view_update_flags):
+                      uint32_t view_update_flags, const Maybe<bool> &is_busy):
             view_(view),
             view_update_flags_(view_update_flags),
+            busy_flag_(is_busy),
             is_full_serialize_(is_full_serialize)
         {}
     };
@@ -133,7 +137,7 @@ class Queue: public QueueIntrospectionIface
     void set_output_stream(std::ostream *os) { active_.dcpd_.set_output_stream(os); }
 
     void add(ViewSerializeBase *view,
-             bool is_full_serialize, uint32_t view_update_flags);
+             bool is_full_serialize, uint32_t view_update_flags, const Maybe<bool> &is_busy);
     bool start_transaction(Mode mode);
 
     bool process_pending_transactions();

src/maybe.hh
@@ -1,0 +1,1 @@
+../strbo_common/src/maybe.hh

src/view_error_sink.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017, 2019, 2022  T+A elektroakustik GmbH & Co. KG
+ * Copyright (C) 2017, 2019, 2022, 2023  T+A elektroakustik GmbH & Co. KG
  *
  * This file is part of DRCPD.
  *
@@ -64,7 +64,8 @@ ViewErrorSink::View::get_dynamic_ids(uint32_t bits) const
 }
 
 bool ViewErrorSink::View::write_xml(std::ostream &os, uint32_t bits,
-                                    const DCP::Queue::Data &data)
+                                    const DCP::Queue::Data &data,
+                                    bool &busy_state_triggered)
 {
     std::lock_guard<std::mutex> lock(errors_lock_);
 
@@ -90,8 +91,10 @@ bool ViewErrorSink::View::write_xml(std::ostream &os, uint32_t bits,
 }
 
 bool ViewErrorSink::View::write_xml_end(std::ostream &os, uint32_t bits,
-                                        const DCP::Queue::Data &data)
+                                        const DCP::Queue::Data &data,
+                                        bool busy_state_triggered)
 {
+    append_busy_value(os, data.busy_flag_, busy_state_triggered);
     os << "</view>";
     return true;
 }

src/view_error_sink.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017--2021  T+A elektroakustik GmbH & Co. KG
+ * Copyright (C) 2017--2021, 2023  T+A elektroakustik GmbH & Co. KG
  *
  * This file is part of DRCPD.
  *
@@ -72,9 +72,9 @@ class View: public ViewIface, public ViewSerializeBase, public Error::Sink
     void defocus() override {}
 
     void update(DCP::Queue &queue, DCP::Queue::Mode mode,
-                std::ostream *debug_os = nullptr) final override
+                std::ostream *debug_os, const Maybe<bool> &is_busy) final override
     {
-        serialize(queue, mode, debug_os);
+        serialize(queue, mode, debug_os, is_busy);
     }
 
     InputResult process_event(UI::ViewEventID event_id,
@@ -96,10 +96,12 @@ class View: public ViewIface, public ViewSerializeBase, public Error::Sink
     get_dynamic_ids(uint32_t bits) const final override;
 
     bool write_xml(std::ostream &os, uint32_t bits,
-                   const DCP::Queue::Data &data) final override;
+                   const DCP::Queue::Data &data,
+                   bool &busy_state_triggered) final override;
 
     bool write_xml_end(std::ostream &os, uint32_t bits,
-                       const DCP::Queue::Data &data) final override;
+                       const DCP::Queue::Data &data,
+                       bool busy_state_triggered) final override;
 };
 
 }

src/view_external_source_base.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017, 2019, 2021, 2022  T+A elektroakustik GmbH & Co. KG
+ * Copyright (C) 2017, 2019, 2021--2023  T+A elektroakustik GmbH & Co. KG
  *
  * This file is part of DRCPD.
  *
@@ -59,7 +59,8 @@ bool ViewExternalSource::Base::is_serialization_allowed() const
 }
 
 bool ViewExternalSource::Base::write_xml(std::ostream &os, uint32_t bits,
-                                         const DCP::Queue::Data &data)
+                                         const DCP::Queue::Data &data,
+                                         bool &busy_state_triggered)
 {
     os << "<text id=\"line0\">" << XmlEscape(_(on_screen_name_)) << "</text>";
     return true;

src/view_external_source_base.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017--2021  T+A elektroakustik GmbH & Co. KG
+ * Copyright (C) 2017--2021, 2023  T+A elektroakustik GmbH & Co. KG
  *
  * This file is part of DRCPD.
  *
@@ -81,7 +81,7 @@ class Base: public ViewIface, public ViewSerializeBase, public ViewWithAudioSour
   protected:
     bool is_serialization_allowed() const override;
     bool write_xml(std::ostream &os, uint32_t bits,
-                   const DCP::Queue::Data &data) override;
+                   const DCP::Queue::Data &data, bool &busy_state_triggered) override;
 };
 
 }

commit e2995724231df3cac639fe9ec2b192cbadcd330d

views: Fix patching of busy flag in pending DCP messages.
Fixes a bug introduced in bd6d3fa66dad27aba97adfe6e009588fc6206d0d.

Change-Id: I0c9ee65914a825989fac30ded1501b14de88d1a2

src/dcp_transaction_queue.cc
@@ -58,12 +58,7 @@ void DCP::Queue::add(ViewSerializeBase *view,
         d->view_update_flags_ |= view_update_flags;
 
         if(is_busy.is_known())
-        {
-            if(d->busy_flag_.is_known())
-                d->busy_flag_.set_unknown();
-            else
-                d->busy_flag_ = is_busy;
-        }
+            d->busy_flag_ = is_busy;
     }
 }
 